181922bd10e607401211329064fb5d61e593f797
Ngireddy <somulanagireddy@gmail.com>
2021-04-18 09:29:24 +0530

    [Nagireddy]: added Hotels to be added

---
+++ src/main/java/Bridgelabz/HotelReservationSystem.java
@@ -0,0 +1,26 @@
+package Bridgelabz;
+
+import java.util.ArrayList;
+
+public class HotelReservationSystem {
+    ArrayList<Hotel> hotelList;
+
+    public HotelReservationSystem() {
+        this.hotelList = new ArrayList<Hotel>();
+    }
+
+    public void addHotel(Hotel hotelToAdd){
+        this.hotelList.add(hotelToAdd);
+    }
+
+    public Hotel getHotelByName(String hotelNameToSearchFor){
+        Hotel hotelToSearchFor = new Hotel();
+        for (Hotel hotel : this.hotelList) {
+            if(hotel.hotelName.equals(hotelNameToSearchFor)){
+                return hotel;
+            }
+        }
+        System.out.println("Hotel with name " + hotelNameToSearchFor + "is not available in our records.");
+        return null;
+    }
+}
